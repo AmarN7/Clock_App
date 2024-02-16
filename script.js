5a6abbe3283c0d72555dc83d6b649e007f261506
AmarN7 <amardalavi63@gmail.com>
2024-02-16 11:44:51 +0530

    Update

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
- function displayTime(){ 
-   
-   let dt = new Date();
-   let hr =(dt.getHours())%12;
-   let mn = dt.getMinutes();
-   let sc = dt.getSeconds();
-   let tm = document.getElementById("time");
-   tm.innerHTML = (hr+":"+mn+":"+sc);
+let hour = document.getElementById('hour')
+let minute = document.getElementById('minute')
+let second = document.getElementById('second')
+
+function displayTime(){
+    let date = new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30* hh + mm/2 + ss/120;
+    let mRotation = 6 * mm + ss/10
+    let sRotation = 6 * ss;
+
+    hour.style.transform = `rotate(${hRotation}deg)`
+    minute.style.transform = `rotate(${mRotation}deg)`
+    second.style.transform = `rotate(${sRotation}deg)`
 }
+
 setInterval(displayTime,1000)
